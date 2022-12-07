80cd621e4cea141fb7edef09cd809cbb0f2dfabb
shahadibrahimAlkhalaf <shahadii3alkhalaf@gmail.com>
2022-12-07 19:38:12 +0300

    Initial Commit

---
+++ Darb/DarbApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarbApp.swift
+//  Darb
+//
+//  Created by Shahad Alkhalaf on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DarbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
